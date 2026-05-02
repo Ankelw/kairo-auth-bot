7d514add7b66c7bb492e8d1cfab3ffde7607c892
Ankelw <andriyt2233@gmail.com>
2026-05-02 18:46:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -52,8 +52,9 @@ def handle_query(call):
                 bot.edit_message_text(f"💳 Счет на {amount} USDT создан!", call.message.chat.id, call.message.message_id, reply_markup=markup)
             else:
                 bot.send_message(call.message.chat.id, f"❌ Ошибка API: {resp.get('error', {}).get('name')}")
-        except:
-            bot.send_message(call.message.chat.id, "Ошибка сервера оплаты.")
+       except Exception as e:
+            # Бот напишет в чат, в чем именно проблема
+            bot.send_message(call.message.chat.id, f"Детальная ошибка: {e}")
 
     elif call.data.startswith("check_"):
         _, inv_id, plan = call.data.split("_")
